56d0e212ad45af202dd462af607c1150a586181b
Chhorpong <129765562+Chhorpong@users.noreply.github.com>
2024-07-07 10:38:36 +0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Login&Register.cpp"
+#include "Application.cpp"
+
+
+using namespace std;
+
+int main()
+{
+
+    Application app;
+    app.run();
+    
+    return 0;
+}
